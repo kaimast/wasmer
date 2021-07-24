b0293108a6fed55327e1357da11d9b0c0a17f81b
Kai Mast <kaimast@cs.wisc.edu>
2021-07-24 17:12:53 -0600

    More complex duplication example

--- examples/duplicate.rs
+++ examples/duplicate.rs
@@ -1,4 +1,4 @@
-use wasmer::{imports, wat2wasm, WasmerEnv, Function, Instance, Module, Store, LazyInit, Memory};
+use wasmer::{Array, imports, wat2wasm, WasmerEnv, WasmPtr, Function, Instance, Module, Store, LazyInit, Memory};
 use wasmer_compiler_llvm::LLVM;
 use wasmer_engine_universal::Universal;
 use wasmer_engine::Engine;
@@ -18,12 +18,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let wasm_bytes = wat2wasm(
         br#"
         (module
+          (func $log (import "" "log") (param i32) (param i32))
           (func $host_function (import "" "host_function") (result i32))
+          (func $print (import "" "host_function") (result i32))
           (memory (export "memory") 1)
+
+          (data "Hi")
+          (data "Goodbye")
 
           (type $my_func_t(func (result i32)))
           (func $my_func_f (type $my_func_t) (result i32)
-           (call $host_function)
+
+          i32.const 16
+
+          (if (result i32) (i32.eq (call $host_function) (i32.const 0))
+              (then
+                  ;; Copy 'hi' to memory 0
+                  (memory.init 0
+                        (i32.const 16)    ;; target offset
+                        (i32.const 0)     ;; source offset
+                        (i32.const 2))    ;; length
+                  (i32.const 2)
+              )
+              (else
+                  ;; Copy 'by' to memory 0
+                  (memory.init 1
+                        (i32.const 16)    ;; target offset
+                        (i32.const 0)     ;; source offset
+                        (i32.const 7))    ;; length
+                  (i32.const 7)
+              )
+           )
+
+           call $log
+
+           ;; Call and return host function
+           call $host_function
           )
           (export "my_func" (func $my_func_f)))
         "#,
@@ -42,19 +72,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let module = Module::new(&store, wasm_bytes)?;
 
     fn host_fn(env: &FnEnv) -> i32 {
-        if env.memory.get_ref().is_none() {
-            panic!("Memory not initialized");
-        }
-
         env.number
     }
 
+    fn log_fn(env: &FnEnv, ptr: WasmPtr<u8, Array>, len: i32) {
+        let memory = env.memory.get_ref().expect("Memory not initialized");
+        let log_msg = ptr.get_utf8_string(memory, len as u32)
+            .expect("Pointer out of bounds");
+
+        println!("Program said: {}", log_msg);
+    }
+
     // Create an import object.
-    let env1 = FnEnv{ number: 42, memory: Default::default() };
-    let host_function1 = Function::new_native_with_env(&store, env1, host_fn);
+    let env1 = FnEnv{ number: 0, memory: Default::default() };
+    let host_function1 = Function::new_native_with_env(&store, env1.clone(), host_fn);
+    let log_function1 = Function::new_native_with_env(&store, env1, log_fn);
     let import_object1 = imports!{
         "" => {
             "host_function" => host_function1,
+            "log" => log_function1,
         },
     };
 
@@ -71,11 +107,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Cloning instance");
     let start = Instant::now();
 
-    let env2 = FnEnv{ number: 1337, memory: Default::default() };
-    let host_function2 = Function::new_native_with_env(&store, env2, host_fn);
+    let env2 = FnEnv{ number: 42, memory: Default::default() };
+    let host_function2 = Function::new_native_with_env(&store, env2.clone(), host_fn);
+    let log_function2 = Function::new_native_with_env(&store, env2, log_fn);
+ 
     let import_object2 = imports!{
         "" => {
             "host_function" => host_function2,
+            "log" => log_function2,
         },
     };
 
@@ -92,8 +131,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let result1: i32 = result1[0].clone().try_into().unwrap();
     let result2: i32 = result2[0].clone().try_into().unwrap();
 
-    assert!(result1 == 42);
-    assert!(result2 == 1337);
+    assert!(result1 == 0);
+    assert!(result2 == 42);
 
     println!("Success");
 
